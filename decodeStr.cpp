fed4ba750db2d09b208b230c3e6eee9de59363b7
Sumit Patel <sumitz1207@gmail.com>
2023-03-24 22:46:44 -0400

    Create decodeStr.cpp

---
+++ decodeStr.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string dec(const string& s, int& i) {
+        string res;
+        
+        while (i < s.length() && s[i] != ']') {
+            if (!isdigit(s[i]))
+                res += s[i++];
+            else {
+                int n = 0;
+                while (i < s.length() && isdigit(s[i]))
+                    n = n * 10 + s[i++] - '0';
+                    
+                i++; // '['
+                string t = dec(s, i);
+                i++; // ']'
+                
+                while (n-- > 0)
+                    res += t;
+            }
+        }
+        
+        return res;
+    }
+
+    string dec(string s) {
+        int i = 0;
+        return dec(s, i);
+    }
+};
