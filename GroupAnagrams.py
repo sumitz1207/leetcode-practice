fcf465242eec31ce43bcb5e42f85ccb858a69e02
Sumit Patel <sumitz1207@gmail.com>
2023-03-29 23:54:04 -0400

    Delete GroupAnagrams.py

--- GroupAnagrams.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-
-class Solution:
-    def ga(self, strs: List[str]) -> List[List[str]]:
-        help = defaultdict(list)
-        for i in strs:
-            help[tuple(sorted(i))].append(i)
-        return list(help.values())
