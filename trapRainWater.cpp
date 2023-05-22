8552e7b54406c2cf8c73dad3727379adf2c168fa
Sumit Patel <sumitz1207@gmail.com>
2023-05-21 23:23:39 -0400

    Delete trapRainWater.cpp (#113)

--- trapRainWater.cpp
+++
@@ -1,9 +0,0 @@
-int trap(int* h, int n) {
-    int lev = 0, fin = 0;
-    while (n--) {
-        int low = *h < h[n] ? *h++ : h[n];
-        if (low > lev) lev = low;
-        fin += lev - low;
-    }
-    return fin;
-}
